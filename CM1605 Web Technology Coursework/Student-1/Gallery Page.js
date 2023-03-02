2e8d5945125b7b90e7e8388646174053958db6bf
Sandushke De Alwis <78974539+Sandushke@users.noreply.github.com>
2023-03-03 00:34:15 +0530

    upload all files

---
+++ CM1605 Web Technology Coursework/Student-1/Gallery Page.js
@@ -0,0 +1,12 @@
+function largeText(x) {
+    x.style.height = "390px";
+    x.style.width = "290px";
+  }
+  
+  function normalText(x) {
+    x.style.height = "360px";
+    x.style.width = "270px";
+  }
+  
+  
+  
---
+++ CM1605 Web Technology Coursework/Student-1/SignupForm.js
@@ -0,0 +1,68 @@
+function signUpForm(){
+    let Full_Name = document.LoginForm.Name.value; 
+    let Address = document.LoginForm.Address.value; 
+    let Email = document.LoginForm.E_mail.value; 
+    let password = document.LoginForm.Password.value;
+    let confirmpassword = document.LoginForm.Confirm_Password.value;
+    let Phone_Number = document.LoginForm.Phone_Number.value;
+    let status = false; 
+    
+    
+    //Check whether the First name box is empty
+    if(Full_Name == ""){  
+        document.getElementById("namelocation-1").innerHTML = "Please enter your first name";  
+        status = false;
+    }else{  
+        document.getElementById("namelocation-1").innerHTML = "";  
+        status = true;
+    }
+
+
+    //Check whether the Last name box is empty
+    if(Address == ""){
+        document.getElementById("namelocation-2").innerHTML = "Please enter your last name";
+        status = false;
+    }else{
+        document.getElementById("namelocation-2").innerHTML = "";
+        status = true;
+    }
+
+
+    //Check whether the Last name box is empty
+    if(Email == ""){
+        document.getElementById("namelocation-3").innerHTML = "Please enter your last name";
+        status = false;
+    }else{
+        document.getElementById("namelocation-3").innerHTML = "";
+        status = true;
+    }
+    
+
+    //Check whether the phone number box is empty
+    if(Phone_Number == ""){
+        document.getElementById("namelocation-4").innerHTML = "Enter a number value";
+        status =  false;
+    }else{
+        status =  true;
+    }
+    
+    
+    if(Full_Name != '' && Address != '' && Email != '' && Phone_Number !='') {
+        if (Phone_Number.length == 10) { 
+            if(password == confirmpassword){
+                alert("Dear " + Full_Name + ", Thank you for using Signing Up with us, the recommended results will be shown in a while.");
+                return true;
+            }
+        }
+    }else{
+        alert("You may need to fill the details")
+    };
+
+    //Check whether the Password box is empty
+    if(password == confirmpassword){
+        return true;
+    }else{
+        document.getElementById("namelocation-5").innerHTML = "Your password dosen't match";
+        return false;
+    }   
+}
